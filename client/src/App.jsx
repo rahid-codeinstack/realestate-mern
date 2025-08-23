691dfa15ab6481fa48f94a04b335fcd1571ab1b4
rahidkhan <rahidkhanbannu1@email.com>
2025-08-24 01:08:09 +0430

    create pages file and route it all

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,31 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import About from "./pages/About"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Header from "./components/Header/Header"
+
 
 
 function App() {
   return (
-    <div className="text-red-500 border-2 border-red-500 text-left">App</div>
+  <BrowserRouter>
+  
+  <Header></Header>
+
+    <Routes>
+
+      <Route path="/" element={<Home></Home>}/>
+      <Route path="/profile" element={<Profile></Profile>}/>
+      <Route path="/about" element={<About></About>}/>
+      <Route path="/sign-in" element={<Signin></Signin>}/>
+      <Route path="/sign-up" element={<Signup></Signup>}/>
+
+
+
+    </Routes>
+  </BrowserRouter>
   )
 }
 
