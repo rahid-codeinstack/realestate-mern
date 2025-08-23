ceaa59569302c4ee50e57f648105fd6a980127bc
rahidkhan <rahidkhanbannu1@email.com>
2025-08-24 02:09:47 +0430

    create header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,7 +3,26 @@ import './Header.css'
 
 function Header() {
   return (
-    <div>Header</div>
+     <>
+
+          <header className='header bg-slate-200 shadow-sm p-3'>
+               <nav className='nav-bar m-auto flex justify-between items-center'>
+                    <h1 className='flex gap-1 lg:text-[25px] font-bold md:text-[25px]'>
+                              <span className='text-slate-500 capitalize '>real</span>     
+                              <span className='text-slate-700 capitalize'>estat</span>
+                    </h1>
+                  <form className='bg-slate-300 justify-start search-form flex  items-center gap-2 py-1 px-3 rounded-sm'>
+                         <input type="text" placeholder='search..' className='focus:outline-none  ' />
+                    </form>
+                   <ul className='flex item-center justify-center gap-3'>
+                    <li className='text-slate-700 hidden md:inline'>Home</li>
+                    <li className='text-slate-700'>About</li>
+                    <li className='text-slate-700'>Profile</li>
+                   </ul>
+               </nav>
+          </header>
+     
+     </>
   )
 }
 
