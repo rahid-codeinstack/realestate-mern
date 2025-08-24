6cfe6fc01a9e38ea0c41f7f728532e8e7be4c9a4
rahidkhan <rahidkhanbannu1@email.com>
2025-08-25 00:58:18 +0430

    create signup route and signup function , and create error meddleware and error handler fucntion

--- server/App.js
+++ server/App.js
@@ -6,7 +6,10 @@ import authRouter from './routes/auth.route.js'
 configDotenv()
 
 const app=express()
-app.use("/api/auth/",authRouter)
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use("/api/auth",authRouter)
+
 const port=process.env.PORT || 3000;
 app.get("/test",(req,res)=>{
      res.send({
@@ -16,9 +19,24 @@ app.get("/test",(req,res)=>{
      )
 })
 
-console.log(process.env)
+
 app.listen(port,()=>{
      console.log("server runing on port 3000")
 })
 
 connectDB(process.env.MONGO_URI)
+
+app.use((err, req, res, next)=>{
+     console.log(err.statusCode,err.message)
+     const statusCode=err.statusCode || 500;
+     const message=err.message || "internal server error";
+
+     res.status(statusCode).json({
+          message,
+          statusCode
+          ,
+          success:false,
+
+     })
+
+})
--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -5,13 +5,13 @@
 
        export async function connectDB(uri){
           try {
-               console.log(uri)
+              
                const res = await mongoose.connect(uri)
                console.log('connection success')
                
           } catch (error) {
                console.log("connection failed")
-               console.log(error)
+               
           }
      }
 
