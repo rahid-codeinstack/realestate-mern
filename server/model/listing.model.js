0f0676867ee8659989166f51c4c6a94135a93c72
rahidkhan <rahidkhanbannu1@email.com>
2025-08-29 11:44:53 +0430

    update list schema

--- server/model/listing.model.js
+++ server/model/listing.model.js
@@ -1,48 +1,86 @@
 import mongoose  from "mongoose";
 const listingSchema=mongoose.Schema({
-          name:{
+      name:{
                type:String,
                required:true,
-          }
-          ,discription:{
+              
+      }
+          
+     , description:{
                type:String,
                required:true,
 
-          },
+     },
           address:{
-               type:String,
-               required:true,
-          },
-          regularprice:{
-                    type:Number,
+               province:{
+                    type:String,
+                    required:true,
+               },
+               city:{
+                    type:String,
                     required:true,
 
+               }
+               ,
+               zipCode:{
+                    type:String,
+                    required:true,
+
+               }
+               ,
+               district:{
+                    type:String,
+                    required:true,
+
+               }
+          },
+          prices:{
+                regularprice: {
+                    type: Number,
+                    required: true,
+
+                 }
+                 ,
+                discountprice: {
+                    type: Number,
+                    required: true,
+
+                }
           }
           ,
-          discountprice:{
-               type:Number,
-               required:true,
+            propertiesfeatures:{
+                 bathroom: {
+                      type: Number,
+                      required: true,
+                    
+                 },
+                 kitchen: {
+                      type: Boolean,
+                      required: true,
+                 },
+                 bedroom: {
+                      type: Number,
+                      required: true,
+                 },
+                 furnished: {
+                      type: Boolean,
+                      required: true,
+                 },
+                 parking: {
+                      type: Boolean,
+                      required: true,
+                 },
+                 elevater: {
+                      type: Boolean,
+                      required: true,
+                 }
+            }   
+          ,
 
-          },
-          bathroom:{
-               type:Number,
-               required:true,
-          },
-          bedroom:{
-               type:Number,
-               required:true,
-          },
-          furnished:{
-               type:Boolean,
-               required:true,
-          },
-          parking:{
-               type:Boolean,
-               required:true,
-          },
           type:{
                type:String,
                required:true,
+               
 
           },
           imageurls:{
